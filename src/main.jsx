583e6bee570cbc317b28f01d4230386f702cfefc
Treehigh1 <155392787+Treehigh1@users.noreply.github.com>
2026-04-03 16:41:29 +1100

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import LotteryBot from './lottery-prediction-bot.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <LotteryBot />
-  </React.StrictMode>
-);
